cb575d19f7bd74f6cf0e64d1dd495eae9ffae0ff
Joe Turner <joe@oampo.co.uk>
2021-09-12 08:32:37 +0100

    Make sure color channels are floored before converting to hex

--- src/data.js
+++ src/data.js
@@ -1,13 +1,22 @@
 import Vec4 from "./vec4";
+
 export function hex(x) {
-  if (x instanceof Vec4) {
-    // Color
-    return (
-      (x.r * 255).toString(16).padStart(2, 0) +
-      (x.g * 255).toString(16).padStart(2, 0) +
-      (x.b * 255).toString(16).padStart(2, 0) +
-      (x.a * 255).toString(16).padStart(2, 0)
-    );
+  if (!(x instanceof Vec4)) {
+    throw new Error(`Could not convert value to hex: ${x}`);
   }
-  throw new Error(`Could not convert value to hex: ${x}`);
+
+  return (
+    Math.floor(x.r * 255)
+      .toString(16)
+      .padStart(2, 0) +
+    Math.floor(x.g * 255)
+      .toString(16)
+      .padStart(2, 0) +
+    Math.floor(x.b * 255)
+      .toString(16)
+      .padStart(2, 0) +
+    Math.floor(x.a * 255)
+      .toString(16)
+      .padStart(2, 0)
+  );
 }
