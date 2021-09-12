3952b5e24db9a24aa4a4ad76ccc730cdfbe23540
Joe Turner <joe@oampo.co.uk>
2021-09-12 08:30:14 +0100

    Add rgba getter/setter functions

--- src/color.js
+++ src/color.js
@@ -5,3 +5,35 @@ export function color(line, color) {
 
   line.color = color;
 }
+
+export function red(line, value) {
+  if (!value) {
+    return line.color.r;
+  }
+
+  line.color.r = value;
+}
+
+export function green(line, value) {
+  if (!value) {
+    return line.color.g;
+  }
+
+  line.color.g = value;
+}
+
+export function blue(line, value) {
+  if (!value) {
+    return line.color.b;
+  }
+
+  line.color.b = value;
+}
+
+export function alpha(line, value) {
+  if (!value) {
+    return line.color.a;
+  }
+
+  line.color.a = value;
+}
