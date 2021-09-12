b96c7535e050df1f1737f7044c79730e7adc06bc
Joe Turner <joe@oampo.co.uk>
2021-09-12 08:33:15 +0100

    Add rgbHex function

--- src/data.js
+++ src/data.js
@@ -20,3 +20,21 @@ export function hex(x) {
       .padStart(2, 0)
   );
 }
+
+export function rgbHex(x) {
+  if (!(x instanceof Vec4)) {
+    throw new Error(`Could not convert value to hex: ${x}`);
+  }
+
+  return (
+    Math.floor(x.r * 255)
+      .toString(16)
+      .padStart(2, 0) +
+    Math.floor(x.g * 255)
+      .toString(16)
+      .padStart(2, 0) +
+    Math.floor(x.b * 255)
+      .toString(16)
+      .padStart(2, 0)
+  );
+}
