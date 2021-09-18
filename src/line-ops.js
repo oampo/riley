8798e158b2607c2f10355f28bf72cd061a0d62ca
Joe Turner <joe@oampo.co.uk>
2021-09-18 08:51:07 +0100

    Add subdivide function

---
+++ src/line-ops.js
@@ -0,0 +1,19 @@
+export function subdivide(line, maxDistance) {
+  const newVertices = [];
+  for (let i = 0; i < line.vertices.length - 1; i++) {
+    const vertexA = line.vertices[i];
+    const vertexB = line.vertices[i + 1];
+    const distance = vertexA.dist(vertexB);
+
+    newVertices.push(vertexA.clone());
+    if (distance > maxDistance) {
+      const verticesBetween = Math.floor(distance / maxDistance);
+      for (let j = 0; j < verticesBetween; j++) {
+        newVertices.push(vertexA.lerp(vertexB, j / (verticesBetween + 1)));
+      }
+    }
+    newVertices.push(vertexB.clone());
+  }
+  line.vertices = newVertices;
+  return line;
+}
