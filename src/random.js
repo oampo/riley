a2894c5d895ccaeb53ff2927926e9c4dd16b236d
Joe Turner <joe@oampo.co.uk>
2021-10-31 16:30:19 +0000

    Add gaussian and randomInt functions

--- src/random.js
+++ src/random.js
@@ -12,3 +12,16 @@ export function random(low = 0, high = 1) {
   }
   return low + rng() * (high - low);
 }
+
+export function randomInt(low=0, high=2) {
+  return Math.floor(random(low, high));
+}
+
+export function gaussian(mean=0, variance=1) {
+  const r1 = random();
+  const r2 = random();
+
+  // Gaussian centered around 0, with variance of 1
+  const gaussian01 = Math.sqrt(-2 * Math.log(r1)) * Math.sin(2 * Math.PI * r2);
+  return mean + variance * gaussian01;
+}
